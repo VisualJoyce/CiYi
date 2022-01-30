20f4fb16c3b8b56d089a96b7b00bbce4b63b8588
TAN Minghuan <vimostan@gmail.com>
2022-01-30 23:30:26 +0800

    add practice and evaluation

--- ciyi/predictors/span_classifier.py
+++ ciyi/predictors/span_classifier.py
@@ -1,7 +1,11 @@
+import logging
+
 from allennlp.common.util import JsonDict
 from allennlp.data import Instance
 from allennlp.predictors.predictor import Predictor
 from overrides import overrides
+
+logger = logging.getLogger(__name__)
 
 
 @Predictor.register('span_classifier')
@@ -10,7 +14,11 @@ class SpanClassifierPredictor(Predictor):
 
     @overrides
     def _json_to_instance(self, json_dict: JsonDict) -> Instance:
-        instance = self._dataset_reader.text_to_instance(json_dict)
+        try:
+            instance = self._dataset_reader.text_to_instance(json_dict)
+        except TypeError:
+            logger.warning(f"Parsing failed: {json_dict}")
+            raise TypeError
         # label_dict = self._model.vocab.get_index_to_token_vocabulary('labels')
         # _ = [label_dict[i] for i in range(len(label_dict))]
         return instance
