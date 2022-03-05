ef945b44de5b2ee1d73f750426384c525dbab495
Visual Joyce <visualjoyce@gmail.com>
2022-03-05 12:34:43 +0800

    add support for subtaska

--- experiments/semeval-2022_task02_idiomacity/subtask_a/predict.sh
+++ experiments/semeval-2022_task02_idiomacity/subtask_a/predict.sh
@@ -6,6 +6,7 @@
 MODEL_NAME=$1
 PHASE_NAME=$2
 SPAN_EXTRACTOR_TYPE=$3
+TRANSFORMER_LAYER=$4
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/root/anaconda3/lib
 
 declare -a datasets=(ZeroShot OneShot)
@@ -14,18 +15,18 @@ declare -a splits=(eval test)
 
 for d in "${datasets[@]}"; do
   for s in "${splits[@]}"; do
-    TRANSFORMER_LAYER=12 ANNOTATION_DIR=data/annotations/semeval-2022_task02_idiomacity/subtask_a/"$PHASE_NAME"/"$d" \
+    TRANSFORMER_LAYER=$TRANSFORMER_LAYER ANNOTATION_DIR=data/annotations/semeval-2022_task02_idiomacity/subtask_a/"$PHASE_NAME"/"$d" \
       allennlp predict \
-      data/output/semeval-2022_task02_idiomacity/SubTaskA/"$PHASE_NAME"/"$d"/finetune/"$MODEL_NAME"/"$SPAN_EXTRACTOR_TYPE"/model.tar.gz \
+      data/output/semeval-2022_task02_idiomacity/SubTaskA/"$PHASE_NAME"/"$d"/finetune/"$MODEL_NAME"/"$SPAN_EXTRACTOR_TYPE"/"$TRANSFORMER_LAYER"/model.tar.gz \
       data/annotations/semeval-2022_task02_idiomacity/subtask_a/"$PHASE_NAME"/"$d"/"$s".jsonl \
       --predictor semeval-2022_task02_idiomacity_subtask_a \
-      --output-file data/output/semeval-2022_task02_idiomacity/SubTaskA/"$PHASE_NAME"/"$d"/finetune/"$MODEL_NAME"/"$SPAN_EXTRACTOR_TYPE"/"$s"_predict.csv \
+      --output-file data/output/semeval-2022_task02_idiomacity/SubTaskA/"$PHASE_NAME"/"$d"/finetune/"$MODEL_NAME"/"$SPAN_EXTRACTOR_TYPE"/"$TRANSFORMER_LAYER"/"$s"_predict.csv \
       --include-package ciyi --cuda-device 0
   done
 done
 
 if [[ $PHASE_NAME == "evaluation" ]]; then
   echo "ID,Language,Setting,Label" >task2_subtaska.csv
-  cat data/output/semeval-2022_task02_idiomacity/SubTaskA/evaluation/ZeroShot/finetune/"$MODEL_NAME"/"$SPAN_EXTRACTOR_TYPE"/test_predict.csv >>task2_subtaska.csv
-  cat data/output/semeval-2022_task02_idiomacity/SubTaskA/evaluation/OneShot/finetune/"$MODEL_NAME"/"$SPAN_EXTRACTOR_TYPE"/test_predict.csv >>task2_subtaska.csv
+  cat data/output/semeval-2022_task02_idiomacity/SubTaskA/evaluation/ZeroShot/finetune/"$MODEL_NAME"/"$SPAN_EXTRACTOR_TYPE"/"$TRANSFORMER_LAYER"/test_predict.csv >>task2_subtaska.csv
+  cat data/output/semeval-2022_task02_idiomacity/SubTaskA/evaluation/OneShot/finetune/"$MODEL_NAME"/"$SPAN_EXTRACTOR_TYPE"/"$TRANSFORMER_LAYER"/test_predict.csv >>task2_subtaska.csv
 fi
